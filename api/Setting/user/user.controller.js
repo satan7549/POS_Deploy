ae039606587e863b9aabcbc84d3e222b454fe9ee
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-06-27 12:38:40 +0530

    Update user.controller.js

--- api/Setting/user/user.controller.js
+++ api/Setting/user/user.controller.js
@@ -1,14 +1,13 @@
-let mongoose = require('mongoose');
-let { validateUser, validateUpdate } = require('./user.validator');
-let UserModel = require('./index');
-let user = require('./index');
-
+let { validateUser, validateUpdate } = require("./user.validator");
+let UserModel = require("./index");
+const cookieToken = require("../../../utils/cookieToken");
 
 //insert new User
-exports.userInsert = async (req,res,next) => {
-    try {
-        // Validation
+exports.userInsert = async (req, res, next) => {
+  try {
+    // Validation
     let { error, value } = validateUser(req.body);
+    console.log("user credentials", value);
 
     // Check Error in Validation
     if (error) {
@@ -20,89 +19,87 @@ exports.userInsert = async (req,res,next) => {
     let savedData = await userModel.save();
 
     // Send Response
-    res.status(200).json('Data inserted');
-    } catch (error) {
 
-      console.log(error);
-       // Send Error Response
-    res.status(500).json('Error inserting data into database'); 
-    }
+    cookieToken(savedData, res);
+    // res.status(200).json('Data inserted');
+  } catch (error) {
+    console.log(error);
+    // Send Error Response
+    res.status(500).json("Error inserting data into database");
+  }
 };
-
 
 // Update User
 exports.updateUser = async (req, res, next) => {
-    try {
-      let id = req.params.id;
-  
-      // Validation
-      let { error, value } = validateUpdate(req.body);
-  
-      // Check Error in Validation
-      if (error) {
-        return res.status(400).send(error.details[0].message);
-      }
-  
-      let user = await UserModel.findOneAndUpdate({ _id: id }, value, {
-        new: true
-      });
-  
-      if (!user) {
-        console.log('User not found');
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      res.status(200).json({ user });
-    } catch (error) {
-  
-      console.log(error);
-      // Send Error Response
-      res.status(500).json('Error updating table');
+  try {
+    let id = req.params.id;
+
+    // Validation
+    let { error, value } = validateUpdate(req.body);
+
+    // Check Error in Validation
+    if (error) {
+      return res.status(400).send(error.details[0].message);
     }
-  };
 
+    let user = await UserModel.findOneAndUpdate({ _id: id }, value, {
+      new: true,
+    });
 
-  // Display Single User
-  exports.showUser = async (req, res, next) => {
-    try {
-      let id = req.params.id;
-      let user = await UserModel.findOne({ _id: id });
-  
-      if (!user) {
-        console.log('user not found');
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      res.status(200).json({ user });
-    } catch (error) {
-      res.status(500).json({ error });
+    if (!user) {
+      console.log("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
-  };
 
-  // Display List
+    res.status(200).json({ user });
+  } catch (error) {
+    console.log(error);
+    // Send Error Response
+    res.status(500).json("Error updating table");
+  }
+};
+
+// Display Single User
+exports.showUser = async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let user = await UserModel.findOne({ _id: id });
+
+    if (!user) {
+      console.log("user not found");
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+// Display List
 exports.showUsers = async (req, res, next) => {
-    try {
-      let user = await UserModel.find();
-      if (!user || user.length === 0) {
-        console.log('User not found');
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json({ user });
-    } catch (error) {
-      res.status(500).json({ error });
+  try {
+    let user = await UserModel.find();
+    if (!user || user.length === 0) {
+      console.log("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
-  };
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
-  // Delete Table
+// Delete Table
 exports.deleteUser = async (req, res, next) => {
   try {
     let id = req.params.id;
 
-   let user = await UserModel.deleteOne({ _id: id });
+    let user = await UserModel.deleteOne({ _id: id });
 
     if (!user) {
-      console.log('User not found');
-      return res.status(404).json({ message: 'User not found' });
+      console.log("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).json({ id });
