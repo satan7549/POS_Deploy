9945a7eb2dd4053f9002c0008cc59eb65c1ead31
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-06-29 16:15:28 +0530

    Update recipeRoute.js

--- api/Setting/recipe/recipeRoute.js
+++ api/Setting/recipe/recipeRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const recipeController = require("./recipeController");
 
 router.post("/new", recipeController.recipeInsert);
-router.post("/list", recipeController.showAllRecipes);
+router.get("/list", recipeController.showAllRecipes);
 router.get("/:id", recipeController.showRecipe);
-router.get("/update/:id", recipeController.updateRecipe);
-router.get("/delete/:id", recipeController.deleteRecipe);
+router.put("/update/:id", recipeController.updateRecipe);
+router.delete("/delete/:id", recipeController.deleteRecipe);
 
 module.exports = router;
