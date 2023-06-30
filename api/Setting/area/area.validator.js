98ba71bb0acf5417372c0453e51e6cf50a0aed4e
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-06-30 15:11:02 +0530

    Update area.validator.js

--- api/Setting/area/area.validator.js
+++ api/Setting/area/area.validator.js
@@ -1,21 +1,21 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 // Define the validation schema
 const areaSchema = Joi.object({
-  outletId: Joi.number().required(),
+  outlet_id: Joi.number().required(),
   areaName: Joi.string().required(),
-  description: Joi.string().allow('').optional(),
+  description: Joi.string().allow("").optional(),
   companyId: Joi.number().optional(),
-  delStatus: Joi.string().allow('').default('Live')
+  delStatus: Joi.string().allow("").default("Live"),
 });
 
 const updateSchema = Joi.object({
-  outletId: Joi.number().required(),
+  outlet_id: Joi.number().required(),
   areaName: Joi.string().required(),
-  description: Joi.string().allow('').optional(),
+  description: Joi.string().allow("").optional(),
   companyId: Joi.number().optional(),
-  delStatus: Joi.string().allow('').default('Live')
-});  
+  delStatus: Joi.string().allow("").default("Live"),
+});
 
 // Validate the area data
 function validateArea(areaData) {
@@ -26,9 +26,7 @@ function validateUpdate(updateData) {
   return updateSchema.validate(updateData);
 }
 
-
 module.exports = {
   validateArea,
-  validateUpdate
-  
+  validateUpdate,
 };
