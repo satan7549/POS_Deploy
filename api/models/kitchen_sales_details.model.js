87ed881b6737fa99c5147eb4bc5c37073f5c3631
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 13:41:49 +0530

    Update kitchen_sales_details.model.js

--- api/models/kitchen_sales_details.model.js
+++ api/models/kitchen_sales_details.model.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const kitchenSalesDetailSchema = new Schema({
-  
   id: { type: Number, required: true },
   food_menu_id: { type: Number, default: null },
   menu_name: { type: String, default: null },
@@ -19,19 +18,26 @@ const kitchenSalesDetailSchema = new Schema({
   discount_amount: { type: Number, default: null },
   item_type: { type: String, default: null },
   cooking_status: { type: String, default: null },
-  cooking_start_time: { type: String, default: '' },
-  cooking_done_time: { type: String, default: '' },
+  cooking_start_time: { type: String, default: "" },
+  cooking_done_time: { type: String, default: "" },
   previous_id: { type: Number, required: true },
   loyalty_point_earn: { type: Number, default: 0 },
   sales_id: { type: Number, default: null },
-  order_status: { type: Number, required: true, comment: '1=new order,2=invoiced order, 3=closed order' },
+  order_status: {
+    type: Number,
+    required: true,
+    comment: "1=new order,2=invoiced order, 3=closed order",
+  },
   user_id: { type: Number, default: null },
   outlet_id: { type: Number, default: null },
   is_free_item: { type: Number, required: true, default: 0 },
   is_print: { type: Number, required: true, default: 1 },
-  del_status: { type: String, default: 'Live' }
+  del_status: { type: String, default: "Live" },
 });
 
-const KitchenSalesDetail = mongoose.model('KitchenSalesDetail', kitchenSalesDetailSchema);
+const KitchenSalesDetail = mongoose.model(
+  "KitchenSalesDetail",
+  kitchenSalesDetailSchema
+);
 
 module.exports = KitchenSalesDetail;
