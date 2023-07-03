1d850749038780988af942b6086d4a4124fddd92
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 13:41:40 +0530

    Update index.js

--- api/Setting/kitchenSales/index.js
+++ api/Setting/kitchenSales/index.js
@@ -3,24 +3,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const kitchenSalesSchema = Schema({
-    delivery_partner_id: {
-        type: Number,
-        default: null,
-      },
-      status: {
-        type: String,
-        required: true,
-        default: 'Pending',
-      },
-      split_sale_id: {
-        type: Number,
-        default: null,
-      },
-      order_receiving_id: {
-        type: Number,
-        required: true,
-        default: 0,
-      }
-})
+  delivery_partner_id: {
+    type: Number,
+    default: null,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Pending",
+  },
+  split_sale_id: {
+    type: Number,
+    default: null,
+  },
+  order_receiving_id: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
 
 module.exports = mongoose.model("KitchenSales", kitchenSalesSchema);
