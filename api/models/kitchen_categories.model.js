247d6c823c829c1441c00d15b0dd0d652b45eb1c
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 13:41:53 +0530

    Update kitchen_categories.model.js

--- api/models/kitchen_categories.model.js
+++ api/models/kitchen_categories.model.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const kitchenCategorySchema = new Schema({
-  
   id: { type: Number, required: true },
   kitchen_id: { type: Number, default: null },
   cat_id: { type: Number, default: null },
   via_printer: { type: Number, default: null },
-  del_status: { type: String, required: true, default: 'Live' },
-  outlet_id: { type: Number, required: true, default: 0 }
+  del_status: { type: String, required: true, default: "Live" },
+  outlet_id: { type: Number, required: true, default: 0 },
 });
 
-const KitchenCategory = mongoose.model('KitchenCategory', kitchenCategorySchema);
+const KitchenCategory = mongoose.model(
+  "KitchenCategory",
+  kitchenCategorySchema
+);
 
 module.exports = KitchenCategory;
