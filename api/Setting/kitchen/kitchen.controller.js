750b0f841cf1786b6f7401a27fc250266cae7888
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 17:22:58 +0530

    Update kitchen.controller.js

--- api/Setting/kitchen/kitchen.controller.js
+++ api/Setting/kitchen/kitchen.controller.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
-const { validateKitchen, validateUpdate} = require('./kitchen.validator');
-const KitchenModel = require('./index');
-
+const { validateKitchen, validateUpdate } = require("./kitchen.validator");
+const KitchenModel = require("./index");
 
 //insert new Kitchen
-
-exports.insertKitchen = async (req,res,next) => {
-    try {
-        // Validation
-    let { error, value } = validateKitchen(req.body);
+exports.insertKitchen = async (req, res, next) => {
+  try {
+    // Validation
+    const { error, value } = validateKitchen(req.body);
 
     // Check Error in Validation
     if (error) {
@@ -16,28 +13,27 @@ exports.insertKitchen = async (req,res,next) => {
     }
 
     // Insert Kitchen
-    let kitchenModel = new KitchenModel(value);
-    let savedData = await kitchenModel.save();
+    const kitchenModel = new KitchenModel(value);
+    const savedData = await kitchenModel.save();
 
     // Send Response
-    res.status(200).json('Data inserted');
-    } catch (error) {
-
-      console.log(error);
-       // Send Error Response
-    res.status(500).json('Error inserting data into database'); 
-    }
+    res.status(200).json("Data inserted");
+  } catch (error) {
+    console.log(error);
+    // Send Error Response
+    res.status(500).json("Error inserting data into database");
+  }
 };
 
 // Display Single Kitchen
 exports.showKitchens = async (req, res, next) => {
   try {
-    let id = req.params.id;
-    let kitchen = await KitchenModel.findOne({ _id: id });
+    const id = req.params.id;
+    const kitchen = await KitchenModel.findOne({ _id: id });
 
     if (!kitchen) {
-      console.log('Kitchen not found');
-      return res.status(404).json({ message: 'Kitchen not found' });
+      console.log("Kitchen not found");
+      return res.status(404).json({ message: "Kitchen not found" });
     }
 
     res.status(200).json({ kitchen });
@@ -49,10 +45,10 @@ exports.showKitchens = async (req, res, next) => {
 // Display List
 exports.showKitchen = async (req, res, next) => {
   try {
-    let kitchen = await KitchenModel.find();
+    const kitchen = await KitchenModel.find();
     if (!kitchen || kitchen.length === 0) {
-      console.log('Kitchen not found');
-      return res.status(404).json({ message: 'Kitchen not found' });
+      console.log("Kitchen not found");
+      return res.status(404).json({ message: "Kitchen not found" });
     }
     res.status(200).json({ kitchen });
   } catch (error) {
@@ -74,20 +70,19 @@ exports.updateKitchen = async (req, res, next) => {
     }
 
     let kitchen = await KitchenModel.findOneAndUpdate({ _id: id }, value, {
-      new: true
+      new: true,
     });
 
     if (!kitchen) {
-      console.log('Kitchen not found');
-      return res.status(404).json({ message: 'Kitchen not found' });
+      console.log("Kitchen not found");
+      return res.status(404).json({ message: "Kitchen not found" });
     }
 
     res.status(200).json({ kitchen });
   } catch (error) {
-
     console.log(error);
     // Send Error Response
-    res.status(500).json('Error updating kitchen');
+    res.status(500).json("Error updating kitchen");
   }
 };
 
@@ -96,11 +91,11 @@ exports.deleteKitchen = async (req, res, next) => {
   try {
     let id = req.params.id;
 
-   let kitchen = await KitchenModel.deleteOne({ _id: id });
+    let kitchen = await KitchenModel.deleteOne({ _id: id });
 
     if (!kitchen) {
-      console.log('Kitchen not found');
-      return res.status(404).json({ message: 'Kitchen not found' });
+      console.log("Kitchen not found");
+      return res.status(404).json({ message: "Kitchen not found" });
     }
 
     res.status(200).json({ id });
