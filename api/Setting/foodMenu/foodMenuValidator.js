94bab698d4972b455275cbe481f67f8981e267f4
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 17:22:49 +0530

    Update foodMenuValidator.js

--- api/Setting/foodMenu/foodMenuValidator.js
+++ api/Setting/foodMenu/foodMenuValidator.js
@@ -1,38 +1,54 @@
 const Joi = require("joi");
 
-const foodMenuSchema = Joi.object({
+const foodMenuJoiSchema = Joi.object({
   name: Joi.string().allow(null).max(50).required(),
-  description: Joi.string().allow(null).max(200).required(),
-  // recipe: Joi.array().items(Joi.string().hex().length(5).required()).required(),
-  recipe: Joi.string().allow(null).max(200).required(),
-  outlet: Joi.object().pattern(
-    /^[a-zA-Z0-9]+$/,
-    Joi.string().hex().length(24).required()
-  ).required(),
-  // company: Joi.string().hex().length(24).required(),
+  code: Joi.string().allow(null).max(50).required(),
+  category: Joi.string().valid("chinese", "mexicon", "indian").required(),
+  ingredients: Joi.array()
+    .items(
+      Joi.object({
+        ingredient: Joi.string().hex().length(24).required(),
+        quantity: Joi.number().required(),
+      })
+    )
+    .required(),
+  price: Joi.number().required(),
+  description: Joi.string().allow(null).max(200),
+  isVeg: Joi.string().valid("yes", "no").required(),
+  isBeverage: Joi.string().valid("yes", "no").required(),
+  outlet: Joi.object().pattern(/^[a-zA-Z0-9]+$/, Joi.string().required()),
+  company: Joi.string().required(),
 });
 
-const updateSchema = Joi.object({
+const updateJoiSchema = Joi.object({
   name: Joi.string().allow(null).max(50).required(),
-  description: Joi.string().allow(null).max(200).required(),
-  // recipe: Joi.array().items(Joi.string().hex().length(5).required()).required(),
-  recipe: Joi.string().allow(null).max(200).required(),
-  outlet: Joi.object().pattern(
-    /^[a-zA-Z0-9]+$/,
-    Joi.string().hex().length(24).required()
-  ).required(),
-  // company: Joi.string().hex().length(24).required(),
+  code: Joi.string().allow(null).max(50).required(),
+  category: Joi.string().valid("chinese", "mexicon", "indian").required(),
+  ingredients: Joi.array()
+    .items(
+      Joi.object({
+        ingredient: Joi.string().hex().length(24).required(),
+        quantity: Joi.number().required(),
+      })
+    )
+    .required(),
+  price: Joi.number().required(),
+  description: Joi.string().allow(null).max(200),
+  isVeg: Joi.string().valid("yes", "no").required(),
+  isBeverage: Joi.string().valid("yes", "no").required(),
+  outlet: Joi.object().pattern(/^[a-zA-Z0-9]+$/, Joi.string().required()),
+  company: Joi.string().required(),
 });
 
 function validateFoodMenu(foodMenuData) {
-  return foodMenuSchema.validate(foodMenuData);
+  return foodMenuJoiSchema.validate(foodMenuData);
 }
 
 function validateUpdate(updateData) {
-  return updateSchema.validate(updateData);
+  return updateJoiSchema.validate(updateData);
 }
 
 module.exports = {
   validateFoodMenu,
-  validateUpdate
+  validateUpdate,
 };
