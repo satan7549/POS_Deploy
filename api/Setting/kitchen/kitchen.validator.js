8721da1788e56e784393292957158f03efaa2d8c
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 13:41:43 +0530

    Update kitchen.validator.js

--- api/Setting/kitchen/kitchen.validator.js
+++ api/Setting/kitchen/kitchen.validator.js
@@ -1,39 +1,34 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const kitchenSchema = Joi.object({
-  
   name: Joi.string().max(250).allow(null),
   printer_id: Joi.number().integer().allow(null),
   print_server_url: Joi.string().max(250).allow(null),
   company_id: Joi.number().integer().allow(null),
-  del_status: Joi.string().max(20).required().default('Live'),
-  outlet_id: Joi.number().integer().default(0)
+  del_status: Joi.string().max(20).required().default("Live"),
+  outlet_id: Joi.number().integer().default(0),
 });
 
 let updateSchema = Joi.object({
-
-    name: Joi.string().max(250).allow(null),
+  name: Joi.string().max(250).allow(null),
   printer_id: Joi.number().integer().allow(null),
   print_server_url: Joi.string().max(250).allow(null),
   company_id: Joi.number().integer().allow(null),
-  del_status: Joi.string().max(20).required().default('Live'),
-  outlet_id: Joi.number().integer().default(0)
+  del_status: Joi.string().max(20).required().default("Live"),
+  outlet_id: Joi.number().integer().default(0),
 });
-
 
 // Validate the table data
 function validateKitchen(kitchenData) {
-    return kitchenSchema.validate(kitchenData);
-  }
+  return kitchenSchema.validate(kitchenData);
+}
 
-  // Validate the update data
+// Validate the update data
 function validateUpdate(updateData) {
-    return updateSchema.validate(updateData);
-  }
+  return updateSchema.validate(updateData);
+}
 
-  module.exports = {
-    validateKitchen,
-    validateUpdate
-    
-  };
-  
+module.exports = {
+  validateKitchen,
+  validateUpdate,
+};
