2ff6c7b5b379a7817b1dbf25fa5e9c91fe0cacaf
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 17:23:07 +0530

    Update kitchen.validator.js

--- api/Setting/kitchen/kitchen.validator.js
+++ api/Setting/kitchen/kitchen.validator.js
@@ -4,9 +4,9 @@ const kitchenSchema = Joi.object({
   name: Joi.string().max(250).allow(null),
   printer_id: Joi.number().integer().allow(null),
   print_server_url: Joi.string().max(250).allow(null),
-  company_id: Joi.number().integer().allow(null),
+  company_id: Joi.string().allow(null).required(),
   del_status: Joi.string().max(20).required().default("Live"),
-  outlet_id: Joi.number().integer().default(0),
+  outlet_id: Joi.string().default(null),
 });
 
 let updateSchema = Joi.object({
