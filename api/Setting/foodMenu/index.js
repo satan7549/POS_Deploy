bc2041d8a3f097f283f91bb9948afc233a4270b6
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-03 17:22:53 +0530

    Update index.js

--- api/Setting/foodMenu/index.js
+++ api/Setting/foodMenu/index.js
@@ -9,26 +9,62 @@ const foodMenuSchema = Schema({
     maxlength: 50,
     required: true,
   },
+  code: {
+    type: String,
+    default: null,
+    maxlength: 50,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    enum: ["chinese", "mexicon", "indian"],
+    required: true,
+  },
+  ingredients: [
+    {
+      ingredient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  price: {
+    type: Number,
+    required: true,
+    default: null,
+  },
   description: {
     type: String,
     default: null,
     maxlength: 200,
   },
-  recipe: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Recipe", required: true },
-  ],
+  isVeg: {
+    type: String,
+    enum: ["yes", "no"],
+    required: true,
+  },
+  isBeverage: {
+    type: String,
+    enum: ["yes", "no"],
+    required: true,
+  },
   outlet: {
     type: Map,
     of: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Outlet",
     },
-    //required: true,
   },
   company: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Company",
-    require: true,
+    required: true,
   },
 });
 
