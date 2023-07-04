bd028fbfa7bafa2bf43add6bd9dcd27e6d83fa3b
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-04 15:46:31 +0530

    Update orderValidator.js

--- api/Setting/order/orderValidator.js
+++ api/Setting/order/orderValidator.js
@@ -1,22 +1,23 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const orderSchema = Joi.object({
-
   persons: Joi.string().required(),
   waiter: Joi.string().required(),
   order_time: Joi.date().required(),
-  order_type: Joi.string().required(),
-  table_id: Joi.string().required(null),
-  del_status: Joi.string().default('Live')
+  order_type: Joi.string().valid("Dine_In", "Take_Away", "Delivery").required(),
+  table_id: Joi.array().items(Joi.string().required()).required(),
+  foodMenu: Joi.array().items(Joi.string().required()).required(),
+  del_status: Joi.string().default("Live"),
 });
 
 const updateSchema = Joi.object({
-    persons: Joi.string().required(),
-    waiter: Joi.string().required(),
-    order_time: Joi.date().required(),
-    order_type: Joi.string().required(),
-    table_id: Joi.string().required(null),
-    del_status: Joi.string().default('Live')
+  persons: Joi.string().required(),
+  waiter: Joi.string().required(),
+  order_time: Joi.date().required(),
+  order_type: Joi.string().valid("Dine_In", "Take_Away", "Delivery").required(),
+  table_id: Joi.array().items(Joi.string().required()).required(),
+  foodMenu: Joi.array().items(Joi.string().required()).required(),
+  del_status: Joi.string().default("Live"),
 });
 
 function validateOrder(orderData) {
@@ -29,5 +30,5 @@ function validateUpdate(updateData) {
 
 module.exports = {
   validateOrder,
-  validateUpdate
+  validateUpdate,
 };
