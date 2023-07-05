c1bb33d1653c799df9751a28363c4f1d8e735c48
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-05 16:12:22 +0530

    Update index.js

--- api/Setting/area/index.js
+++ api/Setting/area/index.js
@@ -6,22 +6,30 @@ const areasSchema = Schema({
   outlet_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Outlet",
-    required: true,
+    required: [true, "please enter outlet_id"],
   },
-  // company_id: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Company",
-  //   required: true,
-  // },
   area_name: {
     type: String,
-    maxlength: 250,
-    default: null,
+    maxlength: [100, "Maximum 100 charcters are permitted"],
+    minLength: [3, "area_name should have more than 3 character"],
+    required: [true, "please enter area_name"],
+    trim: true,
+    unique: true,
   },
   description: {
     type: String,
-    maxlength: 100,
+    maxlength: [100, "Maximum 100 charcters are permitted"],
+    minLength: [5, "description should have more than 5 character"],
     default: null,
+    trim: true,
+  },
+  del_status: {
+    type: String,
+    enum: {
+      values: ["Live", "deactivate"],
+      message: "Value is not matched",
+    },
+    default: "Live",
   },
 });
 
