0ac9bae9c785b21d70dc27b4e0dd5ad49c2775a0
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-05 16:55:29 +0530

    Update Company.js

--- api/Setting/Company/Company.js
+++ api/Setting/Company/Company.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 
 const CompanySchema = new mongoose.Schema({
-    name:{
+  name: {
     type: String,
     maxlength: [30, "Maximum 30 charcters are permitted"],
-    minLength: [3, "name should have more than 3 character"],
-    required: [true,"please enter name"],
+    minLength: [3, "Company name should have more than 3 character"],
+    required: [true, "please enter name"],
+    unique: true,
     trim: true,
-    },
+  },
 
-    cuisine: {
+  cuisine: {
     type: String,
     maxlength: [50, "Maximum 50 charcters are permitted"],
-    minLength: [5, "name should have more than 5 character"],
-    required: [true,"please enter cuisine"],
+    minLength: [5, "cusine should have more than 5 character"],
     trim: true,
-    },
+  },
 
-    // Array of outlet references
-    outlets: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Outlet'
-    }],
-    
-    del_status: {
-        type: String,
-        enum: {
-          values: ["activate","deactivate"],
-          message: `activate or deactivate`,
-        },
-        default: "Live",
-      },
+  // Array of outlet references
+  outlets: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Outlet",
+    },
+  ],
+
+  del_status: {
+    type: String,
+    enum: {
+      values: ["Live", "Deactivate"],
+      message: "Value is not matched",
+    },
+    default: "Live",
+  },
 });
 
 module.exports = mongoose.model("Company", CompanySchema);
