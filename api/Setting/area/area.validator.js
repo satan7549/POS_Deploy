3e0ad92cb57e59418826678d90bb4d4a4a4998d8
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-05 16:12:32 +0530

    Update area.validator.js

--- api/Setting/area/area.validator.js
+++ api/Setting/area/area.validator.js
@@ -2,19 +2,27 @@ const Joi = require("joi");
 
 // Define the validation schema
 const areaSchema = Joi.object({
-  outlet_id: Joi.string().required(),
-  area_name: Joi.string().required(),
-  description: Joi.string().allow("").optional(),
-  // companyId: Joi.number().optional(),
-  delStatus: Joi.string().allow("").default("Live"),
-});
-
-const updateSchema = Joi.object({
-  outlet_id: Joi.string().required(),
-  area_name: Joi.string().required(),
-  description: Joi.string().allow("").optional(),
-  // companyId: Joi.number().optional(),
-  delStatus: Joi.string().allow("").default("Live"),
+  outlet_id: Joi.string().required().messages({
+    "any.required": "Outlet ID is required",
+    "string.empty": "Outlet ID must not be empty",
+  }),
+  area_name: Joi.string().required().min(3).max(100).messages({
+    "any.required": "Area name is required",
+    "string.empty": "Area name must not be empty",
+    "string.min": "Area name should have at least {100} characters",
+    "string.max": "Area name should have at most {3} characters",
+  }),
+  description: Joi.string().allow("").optional().min(5).max(100).messages({
+    "string.min": "Description should have at least {100} characters",
+    "string.max": "Description should have at most {5} characters",
+  }),
+  delStatus: Joi.string()
+    .allow("")
+    .valid("Live", "deactivate")
+    .default("Live")
+    .messages({
+      "any.only": "Value is not matched",
+    }),
 });
 
 // Validate the area data
@@ -23,7 +31,7 @@ function validateArea(areaData) {
 }
 
 function validateUpdate(updateData) {
-  return updateSchema.validate(updateData);
+  return areaSchema.validate(updateData);
 }
 
 module.exports = {
