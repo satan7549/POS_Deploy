ea461994d91c5e01fc2dfc00494614d44ee9925a
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-05 16:55:38 +0530

    Update CompanyController.js

--- api/Setting/Company/CompanyController.js
+++ api/Setting/Company/CompanyController.js
@@ -1,83 +1,91 @@
-const Company = require("./Company");
-const Outlet = require("../outlet/index");
+const CompanyModel = require("./Company");
 
 // Create a new company
-exports.createCompany = (req, res) => {
-  const { name, cuisine } = req.body;
+exports.createCompany = async (req, res) => {
+  const { name } = req.body;
 
-  const company = new Company({
-    name,
-    cuisine,
-  });
+  try {
+    const companyExists = await CompanyModel.findOne({ name: name });
 
-  company
-    .save()
-    .then(() => {
-      res.status(201).json({ message: "Company created successfully" });
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Failed to create company" });
-    });
+    if (companyExists) {
+      return req.status(409).json({ message: "Company Already Exists!" });
+    }
+
+    const newCompany = new CompanyModel(req.body);
+    const company = await newCompany.save();
+    res.status(201).json({ message: "Company created successfully", company });
+  } catch (error) {
+    req.status(500).json({ error: "Failed to create company" });
+  }
 };
 
 // Get all companys
-exports.getCompanys = (req, res) => {
-  Company.find()
-    .then((companys) => {
-      res.json(companys);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Failed to retrieve companys" });
-    });
+exports.getCompanys = async (req, res) => {
+  try {
+    const Company = await CompanyModel.find({ del_status: "Live" });
+
+    if (!Company || Company.length === 0) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+
+    res.status(200).json({ message: "success", Company });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 // Get a specific company by ID
-exports.getCompanyById = (req, res) => {
-  const companyId = req.params.id;
+exports.getCompanyById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const Company = await CompanyModel.findById(id);
 
-  Company.findById(companyId)
-    .then((company) => {
-      if (!company) {
-        return res.status(404).json({ error: "Company not found" });
-      }
+    if (!Company) {
+      return res.status(404).json({ message: "Company not found" });
+    }
 
-      res.json(company);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Failed to retrieve company" });
-    });
+    res.status(200).json({ message: "success", Company });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 // Update a company
-exports.updateCompany = (req, res) => {
-  const companyId = req.params.id;
+exports.updateCompany = async (req, res) => {
+  const id = req.params.id;
   const { name, cuisine } = req.body;
 
-  Company.findByIdAndUpdate(companyId, { name, cuisine }, { new: true })
-    .then((company) => {
-      if (!company) {
-        return res.status(404).json({ error: "Company not found" });
+  try {
+    const company = await CompanyModel.findOneAndUpdate(
+      { _id: id },
+      { name, cuisine },
+      {
+        new: true,
       }
+    );
 
-      res.json(company);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: "Failed to update company" });
-    });
+    if (!company) {
+      return res.status(404).json({ message: "Company not found" });
+    }
+    res.status(200).json({ message: "success", company });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update company" });
+  }
 };
 
 // Delete a company
 exports.deleteCompany = async (req, res) => {
-  const companyId = req.params.id;
+  const id = req.params.id;
   try {
-    const companyExists = await Company.findById(companyId);
+    const companyExists = await CompanyModel.findById(id);
+
     if (!companyExists) {
       return res.status(404).json({ error: "Company not found" });
     }
 
     //Update del_status to "Deactivate"
     companyExists.del_status = "deactivate";
-    await Company.save();
+    await CompanyModel.save();
 
     res.json({ message: "Company deleted successfully" });
   } catch (error) {
@@ -88,16 +96,16 @@ exports.deleteCompany = async (req, res) => {
 // Retrieve all outlets for a company
 exports.getOutletsForCompany = async (req, res) => {
   try {
-    const companyId = req.params.companyId;
+    const id = req.params.companyId;
 
-    const company = await Company.findById(companyId).populate("outlets");
+    const company = await CompanyModel.findById(id).populate("outlets");
 
     if (!company) {
       return res.status(404).json({ error: "Company not found" });
     }
 
     const outlets = company.outlets;
-    res.json(outlets);
+    res.status(200).json({ message: "success", outlets });
   } catch (error) {
     res.status(500).json({ error: "Failed to retrieve company outlets" });
   }
