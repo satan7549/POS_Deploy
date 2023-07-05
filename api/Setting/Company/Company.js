4014b9ff8a190ff61fc9d9264914cde944cd5e2f
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-05 11:30:28 +0530

    Update Company.js

--- api/Setting/Company/Company.js
+++ api/Setting/Company/Company.js
@@ -1,13 +1,36 @@
 const mongoose = require("mongoose");
 
 const CompanySchema = new mongoose.Schema({
-    name: String,
-    cuisine: String,
+    name:{
+    type: String,
+    maxlength: [30, "Maximum 30 charcters are permitted"],
+    minLength: [3, "name should have more than 3 character"],
+    required: [true,"please enter name"],
+    trim: true,
+    },
+
+    cuisine: {
+    type: String,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "name should have more than 5 character"],
+    required: [true,"please enter cuisine"],
+    trim: true,
+    },
+
     // Array of outlet references
     outlets: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Outlet'
-    }]
+    }],
+    
+    del_status: {
+        type: String,
+        enum: {
+          values: ["activate","deactivate"],
+          message: `activate or deactivate`,
+        },
+        default: "Live",
+      },
 });
 
 module.exports = mongoose.model("Company", CompanySchema);
