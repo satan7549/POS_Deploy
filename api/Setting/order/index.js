152a03e52ee90bfd4647de514d4d2f6372c55524
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-06 09:41:24 +0530

    Update index.js

--- api/Setting/order/index.js
+++ api/Setting/order/index.js
@@ -5,30 +5,44 @@ const Schema = mongoose.Schema;
 const orderSchema = Schema({
   persons: {
     type: String,
-    required: true,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "persons should have more than 5 character"],
+    required: [true, "please enter persons"],
+    trim: true,
   },
+
   waiter: {
     type: String,
-    required: true,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "waiter should have more than 5 character"],
+    required: [true, "please enter waiter"],
+    trim: true,
   },
+
   order_time: {
     type: Date,
-    required: true,
+    default: Date.now,
   },
+
   order_type: {
     type: String,
     enum: {
       values: ["Dine_In", "Take_Away", "Delivery"],
-      message: `Value is not supported`,
+      message: "Value is not supported",
     },
-    required: true,
+    required: [true, "please enter order_type"],
   },
+
   table_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "Table" }],
   
   foodMenu: [{ type: mongoose.Schema.Types.ObjectId, ref: "FoodMenu" }],
 
   del_status: {
     type: String,
+    enum: {
+      values: ["Live", "deactivate"],
+      message: "Value is not matched",
+    },
     default: "Live",
   },
 });
