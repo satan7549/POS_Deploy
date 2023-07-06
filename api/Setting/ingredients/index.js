0110060c9e1193f27313d2a715c93a1acfc3ae44
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-06 09:41:35 +0530

    Update index.js

--- api/Setting/ingredients/index.js
+++ api/Setting/ingredients/index.js
@@ -5,23 +5,41 @@ const Schema = mongoose.Schema;
 const ingredientSchema = Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
-    default: null,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "name should have more than 5 character"],
+    required: [true, "please enter name"],
+    trim: true,
+
   },
   category: {
     type: String,
-    default: null,
+    maxlength: [100, "Maximum 100 charcters are permitted"],
+    minLength: [10, "category should have more than 10 character"],
+    required: [true, "please enter category"],
+    trim: true,
+  
   },
   unit: {
     type: String,
-    required: true,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "unit should have more than 5 character"],
+    required: [true, "please enter unit"],
+    trim: true,
     default: null,
   },
   costUnit: {
     type: Number,
-    required: true,
+    required: [true, "please enter costUnit"],
     default: null,
+  },
+
+  del_status: {
+    type: String,
+    enum: {
+      values: ["Live", "deactivate"],
+      message: "Value is not matched",
+    },
+    default: "Live",
   },
   //   recipe: {
   //     type: mongoose.Schema.Types.ObjectId,
