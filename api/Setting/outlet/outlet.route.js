dc12068a4d630d98286997ff44a6cbca689dd3de
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-08 15:46:59 +0530

    Update outlet.route.js

--- api/Setting/outlet/outlet.route.js
+++ api/Setting/outlet/outlet.route.js
@@ -1,26 +1,24 @@
-let express = require('express');
-let router = express.Router(); // access the method of route
+const express = require("express");
+const router = express.Router(); // access the method of route
 
-let outletController = require('./outlet.controller');
+const outletController = require("./outlet.controller");
 
 // Insert
-router.post('/new', outletController.outletInsert);
+router.post("/new", outletController.outletInsert);
 
 //Show List
-router.get('/list', outletController.showAllOutlets);
+router.get("/list", outletController.showAllOutlets);
 
 //Display one single Detail
-router.get('/show/:id', outletController.showSingleOutlet);
+router.get("/show/:id", outletController.showSingleOutlet);
 
 //Update single Details
-router.put('/update/:id', outletController.updateOutlet);
+router.put("/update/:id", outletController.updateOutlet);
 
 //Delete single Details
-router.delete('/delete/:id', outletController.deleteOutlet);
-
+router.delete("/delete/:id", outletController.deleteOutlet);
 
 // Find Company for Test
-router.get('/find-company', outletController.findCompanyByOutletId)
-
+router.get("/find-company", outletController.findCompanyByOutletId);
 
 module.exports = router;
