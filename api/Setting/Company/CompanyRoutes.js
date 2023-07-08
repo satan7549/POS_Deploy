0cbb5f947bbd30c9a4fe33648969159b2d73dba9
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-08 16:01:04 +0530

    Update CompanyRoutes.js

--- api/Setting/Company/CompanyRoutes.js
+++ api/Setting/Company/CompanyRoutes.js
@@ -1,23 +1,25 @@
-const express = require("express");
+
+const express = require('express');
 const router = express.Router();
-const CompanyController = require("./CompanyController.js");
+const CompanyController = require('./CompanyController.js');
 
 // Create a new company
-router.post("/new", CompanyController.outletInsert);
+router.post('/new', CompanyController.createCompany);
 
 // Get all companies
-router.get("/list", CompanyController.showAllOutlets);
+router.get('/list', CompanyController.getCompanys);
 
 // Get a specific company by ID
-router.get("/show/:id", CompanyController.showSingleOutlet);
+router.get('/show/:id', CompanyController.getCompanyById);
 
 // Get all outlets for a company
-router.get("/:id/outlets", CompanyController.findCompanyByOutletId);
+router.get('/:id/outlets', CompanyController.getOutletsForCompany);
 
 // Update a company
-router.put("/update/:id", CompanyController.updateOutlet);
+router.put('/update/:id', CompanyController.updateCompany);
 
 // Delete a company by id
-router.delete("/delete/:id", CompanyController.deleteOutlet);
+router.delete('/delete/:id', CompanyController.deleteCompany);
+
 
 module.exports = router;
