cf0d1e5c7c900e5f6a386aead2ca5ee6b26e5ec6
rajan-maury <rajan.maurya@furation.tech>
2023-07-10 12:17:24 +0530

    changes in index and validator

--- api/Setting/ingredients/index.js
+++ api/Setting/ingredients/index.js
@@ -7,31 +7,57 @@ const ingredientSchema = Schema({
     type: String,
     maxlength: [50, "Maximum 50 charcters are permitted"],
     minLength: [5, "name should have more than 5 character"],
-    required: [true, "please enter name"],
+    required: [true, "please enter IngredientName"],
     trim: true,
+  },
 
-  },
-  category: {
-    type: String,
-    maxlength: [100, "Maximum 100 charcters are permitted"],
-    minLength: [10, "category should have more than 10 character"],
-    required: [true, "please enter category"],
-    trim: true,
-  
-  },
-  unit: {
-    type: String,
-    maxlength: [50, "Maximum 50 charcters are permitted"],
-    minLength: [5, "unit should have more than 5 character"],
-    required: [true, "please enter unit"],
-    trim: true,
+  code: {
+    type: Number,
+    required: [true, "please enter code"],
     default: null,
   },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "IngredientCategory",
+    required: [true, "please enter IngredientCategory"],
+  },
+
+  PurchaseUnit: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "IngredientUnit",
+    required: [true, "please enter PurchaseUnit"],
+  },
+
+  ConsumptionUnit: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "IngredientUnit",
+    required: [true, "please enter ConsumptionUnit"],
+  },
+
+  ConversionRate: {
+    type: Number,
+    required: [true, "please enter ConversionRate"],
+    default: null,
+  },
+
+  PurchaseRate: {
+    type: Number,
+    required: [true, "please enter PurchaseRate"],
+    default: null,
+  },
+
   costUnit: {
     type: Number,
     required: [true, "please enter costUnit"],
     default: null,
   },
+
+  LowQty: {
+    type: Number,
+    required: [true, "please enter LowQty"],
+    default: null,
+  }, 
 
   del_status: {
     type: String,
