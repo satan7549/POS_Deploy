07fa8915589b42be8d9a7b3419ae27c58facab2d
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-10 11:35:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const settingIngredientRouter = require("./api/Setting/ingredients/ingredient.ro
 const FoodCategory = require("./api/Setting/foodCategory/foodCategory.route");
 const FoodMenurouter = require("./api/Setting/foodMenu/foodMenuRoute");
 const Companyrouter = require("./api/Setting/Company/CompanyRoutes");
-const Orderrouter = require("./api/Setting/order/orderRoute");
+const orederRoutes = require("./api/Setting/order/orderRoute");
 const IngredientCategoryrouter = require("./api/Setting/ingredientCategory/ingredientCategoryRoute");
 const IngredientUnitrouter = require("./api/Setting/ingredientUnit/ingredientUnitRoute");
 
@@ -69,7 +69,7 @@ app.use("/setting/ingredient", settingIngredientRouter);
 //app.use("/setting/attendance", settingAttendanceRouter);
 app.use("/setting/foodcategory", FoodCategory);
 app.use("/setting/foodMenu", FoodMenurouter);
-app.use("/setting/order", Orderrouter);
+app.use("/setting/order", orederRoutes);
 app.use("/setting/ingredientCategory", IngredientCategoryrouter);
 app.use("/setting/ingredientUnit",IngredientUnitrouter);
 
