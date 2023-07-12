6a97a688a308c01455880d368f6dec8476fe343e
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-07-12 12:45:38 +0530

    user schema and authorization

--- api/Setting/user/index.js
+++ api/Setting/user/index.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
+  outlet_id: {
+    type: Number,
+    default: null,
+  },
+
   full_name: {
     type: String,
     maxlength: [50, "Maximum 50 charcters are permitted"],
@@ -23,7 +24,7 @@ const userSchema = Schema({
     default: null,
   },
 
-  email_address: {
+  email: {
     type: String,
     required: [true, "please enter email"],
     trim: true,
@@ -31,20 +32,18 @@ const userSchema = Schema({
     unique: true,
     validate: [validator.isEmail, "please enter valid email"],
   },
+
   password: {
     type: String,
     minLength: [6, "Password should have more than 6 character"],
     required: [true, "please enter password"],
     trim: true,
   },
-  company_id: {
-    type: String,
-    required: true,
-  },
+
   role: {
     type: String,
     enum: {
-      values: ["Super_Admin", "Admin", "Employee", "Customer", ],
+      values: ["Admin", "Employee", "Customer", "SuperAdmin", ],
       message: `Value is not supported`,
     },
     default: "Customer",
@@ -56,98 +55,16 @@ const userSchema = Schema({
     default: null,
   },
 
-  will_login: {
-    type: String,
-    default: "No",
+  admin_id: {
+    type: Number,
+    default: null,
+    required: true,
   },
 
-  outlet_id: {
-    type: Number,
-    default: null,
-  },
-  outlets: {
-    type: String,
-    default: null,
-  },
-  kitchens: {
-    type: String,
-    default: null,
-  },
-  account_creation_date: {
-    type: String,
-    default: null,
-  },
-  language: {
-    type: String,
-    default: "english",
-  },
-  last_login: {
-    type: String,
-    default: null,
-  },
-  created_id: {
-    type: Number,
-    default: null,
-  },
-  active_status: {
-    type: String,
-    default: "Active",
-  },
-  del_status: {
-    type: String,
-    default: "Active",
-  },
-  question: {
-    type: String,
-    default: null,
-  },
-  answer: {
-    type: String,
-    default: null,
-  },
-  login_pin: {
-    type: String,
-    default: null,
-  },
-  order_receiving_id: {
-    type: Number,
-    default: 0,
-  },
-  role_id: {
-    type: Number,
-    default: null,
-  },
-
-  forgotPasswardToken: String,
-  forgotPasswardExpiry: Date,
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
-
-//encrypt password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  this.password = await bcrypt.hash(this.password, 10);
-});
-
-//validate the password with passedon user password
-userSchema.methods.isValidatedPassword = async function (userSendPasswrord) {
-  return await bcrypt.compare(userSendPasswrord, this.password);
-};
-
-//create and return JWT token
-userSchema.methods.getToken = async function () {
-  return jwt.sign({
-      id: this._id,
-    },
-    process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRY,
-    }
-  );
-};
 
 module.exports = mongoose.model("User", userSchema);
