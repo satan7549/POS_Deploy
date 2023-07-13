85c3e61fe3238f77c4da5d6fc46b3619514398b3
Survesh Pandit <Monu.Doc3@gmail.com>
2023-07-13 15:10:06 +0530

    billing done

---
+++ api/Setting/billing/billing.validator.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const billingSchema = Joi.object({
+  billing_name: Joi.string().required(),
+  userID: Joi.string().required(),
+  billingDate: Joi.date().required(),
+  totalAmount: Joi.number().required(),
+  paymentMethod: Joi.string().required(),
+  transactionStatus: Joi.string().required(),
+  del_status: Joi.string().valid("Live", "Deleted").default("Live")
+});
+
+const updateSchema = Joi.object({
+  billing_name: Joi.string().required(),
+  userID: Joi.string().required(),
+  billingDate: Joi.date().required(),
+  totalAmount: Joi.number().required(),
+  paymentMethod: Joi.string().required(),
+  transactionStatus: Joi.string().required(),
+  del_status: Joi.string().valid("Live", "Deleted").default("Live")
+});
+
+function validateBilling(billingData) {
+  return billingSchema.validate(billingData);
+}
+
+function validateUpdate(updateData) {
+  return updateSchema.validate(updateData);
+}
+
+module.exports = {
+  validateBilling,
+  validateUpdate,
+};
---
+++ api/Setting/billing/index.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const billingSchema = Schema({
+  billing_name: {
+    type: String,
+    required: true,
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  billingDate: {
+    type: Date,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  transactionStatus: {
+    type: String,
+    required: true,
+  },
+  del_status: {
+    type: String,
+    enum: {
+      values: ["Live", "Deleted"],
+      message: "Values is not matched",
+    },
+    default: "Live",
+  },
+});
+
+module.exports = mongoose.model("Billing", billingSchema);
