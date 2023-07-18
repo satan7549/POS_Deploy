17b67cec3c08a88c1d8a872f037741fd9f54a1b9
rajan-maury <rajan.maurya@furation.tech>
2023-07-18 16:09:04 +0530

    changes in modofier

--- api/Setting/modifiers/index.js
+++ api/Setting/modifiers/index.js
@@ -11,6 +11,22 @@ const modifierSchema = Schema({
     trim: true,
     default: null,
   },
+  ingredients: [
+    {
+      ingredient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient",
+        required: [true, "please enter ingredient"],
+      },
+
+      quantity: {
+        type: Number,
+        maxlength: [10, "Maximum 10 charcters are permitted"],
+        minLength: [1, "quantity should have more than 1 character"],
+        required: [true, "please enter quantity"],
+      },
+    },
+  ],
   price: {
     type: Number,
     required: [true, "Please enter a modifier_price"],
@@ -18,8 +34,6 @@ const modifierSchema = Schema({
   description: {
     type: String,
     maxlength: [100, "Maximum 100 charcters are permitted"],
-    minLength: [10, "modifier_description should have more than 10 character"],
-    required: [true, "please enter modifier_description"],
     trim: true,
     default: null,
   },
@@ -29,22 +43,7 @@ const modifierSchema = Schema({
     ref: "Company",
     required: [true, "please enter Company_id"],
   },
-  // tax_information: {
-  //   type: String,
-  //   maxlength: [100, "Maximum 100 charcters are permitted"],
-  //   minLength: [10, "tax_information should have more than 10 character"],
-  //   required: [true, "please enter tax_information"],
-  //   trim: true,
-  //   default: null
-  // },
-  // tax_string: {
-  //   type: String,
-  //   maxlength: [250, "Maximum 250 charcters are permitted"],
-  //   minLength: [10, "tax_string should have more than 10 character"],
-  //   required: [true, "please enter tax_string"],
-  //   trim: true,
-  //   default: null
-  // },
+  
   total_cost: {
     type: Number,
     required: [true, "Please enter a total_cost"],
