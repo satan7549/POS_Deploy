ef447775232f3dd96e1f7dd139186ac4bdf40b60
rajan-maury <rajan.maurya@furation.tech>
2023-07-18 15:55:37 +0530

    billing schema commit

--- api/Setting/billing/index.js
+++ api/Setting/billing/index.js
@@ -3,13 +3,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const billingSchema = Schema({
-  billing_name: { type: String, required: true, },
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, },
-  billingDate: { type: Date, required: true, },
-  totalAmount: { type: Number, required: true,  },
-  paymentMethod: { type: String, required: true, },
-  transactionStatus: { type: String, required: true, },
-  del_status: { type: String, enum: { values: ["Live", "Deleted"], message: "Values is not matched", }, default: "Live", },
+  billing_name: {
+    type: String,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [5, "billing_name should have more than 5 character"],
+    required: [true, "please enter billing_name"],
+    trim: true,
+    default: null,
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "please enter userID"],
+  },
+  billingDate: {
+    type: Date,
+    default: Date.now,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    maxlength: [50, "Maximum 50 charcters are permitted"],
+    minLength: [3, "paymentMethod name should have more than 3 character"],
+    required: [true, "please enter paymentMethod"],
+    trim: true,
+  },
+  transactionStatus: {
+    type: String,
+    enum: {
+      values: ["Pending", "Done", "Failed"],
+      message: "Values is not matched",
+    },
+    default: "Done",
+  },
+  del_status: {
+    type: String,
+    enum: {
+      values: ["Live", "Deleted"],
+      message: "Values is not matched",
+    },
+    default: "Live",
+  },
 });
 
 module.exports = mongoose.model("Billing", billingSchema);
