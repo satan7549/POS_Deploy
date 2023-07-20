5689c816690b5fd67bf9c3f34348bd01564b8497
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-07-20 17:25:55 +0530

    user RBAMS update by pavan

--- api/Setting/user/user.controller.js
+++ api/Setting/user/user.controller.js
@@ -1,13 +1,11 @@
-const {
-  validateUser,
-  validateUpdate
-} = require("./user.validator");
-const {userModel} = require("./index");
+const { validateUser, validateUpdate } = require("./user.validator");
+const { userModel } = require("./index");
 // const companyModel = require("../Company/index");
 const cookieToken = require("../../../utils/cookieToken");
 const CustomError = require("../../../utils/customError");
 const OutletModel = require("../outlet/index");
 
+const jwt = require("jsonwebtoken");
 //insert new User
 exports.userInsert = async (req, res, next) => {
   try {
@@ -46,9 +44,9 @@ exports.userInsert = async (req, res, next) => {
 
     // Insert user
     // Find the associated outlet by ID
- 
+
     const outlet = await OutletModel.find({
-      outlet_code: req.body.outlet_code //values
+      outlet_code: req.body.outlet_code, //values
     });
 
     const UserModel = new userModel(req.body); //value
@@ -56,12 +54,11 @@ exports.userInsert = async (req, res, next) => {
 
     // Send Response
     cookieToken(savedData, res);
-    
   } catch (error) {
     // Send Error Response
     res.status(500).json({
-      message: "Error inserting data into database",
-      error: error.message
+      message: "Something went wrong",
+      error: error.message,
     });
   }
 };
@@ -72,35 +69,38 @@ exports.updateUser = async (req, res, next) => {
     const id = req.params.id;
 
     // Validation
-    const {
-      error,
-      value
-    } = validateUpdate(req.body);
+    const { error, value } = validateUpdate(req.body);
 
     // Check Error in Validation
     if (error) {
       return res.status(400).send(error.details[0].message);
     }
 
-    const user = await userModel.findOneAndUpdate({
-      _id: id
-    }, value, {
-      new: true,
-    });
+    const user = await userModel.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      value,
+      {
+        new: true,
+      }
+    );
 
     if (!user) {
       return res.status(404).json({
-        message: "User not found"
+        message: "User not found",
       });
     }
 
     res.status(200).json({
       message: "success",
-      user
+      user,
     });
   } catch (error) {
     // Send Error Response
-    res.status(500).json("Error updating user");
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: error.message });
   }
 };
 
@@ -109,22 +109,23 @@ exports.showUser = async (req, res, next) => {
   try {
     const id = req.params.id;
     const user = await userModel.findOne({
-      _id: id
+      _id: id,
     });
 
     if (!user) {
       console.log("user not found");
       return res.status(404).json({
-        message: "User not found"
+        message: "User not found",
       });
     }
 
     res.status(200).json({
-      user
+      user,
     });
   } catch (error) {
     res.status(500).json({
-      error
+      message: "Something went wrong",
+      error: error.message,
     });
   }
 };
@@ -136,15 +137,16 @@ exports.showUsers = async (req, res) => {
     if (!user || user.length === 0) {
       console.log("User not found");
       return res.status(404).json({
-        message: "User not found"
+        message: "User not found",
       });
     }
     res.status(200).json({
-      user
+      user,
     });
   } catch (error) {
     res.status(500).json({
-      error
+      message: "Something went wrong",
+      error: error.message,
     });
   }
 };
@@ -155,42 +157,42 @@ exports.deleteUser = async (req, res, next) => {
     const id = req.params.id;
 
     const user = await userModel.deleteOne({
-      _id: id
+      _id: id,
     });
 
     if (!user) {
       console.log("User not found");
       return res.status(404).json({
-        message: "User not found"
+        message: "User not found",
       });
     }
 
     res.status(200).json({
-      id
+      id,
     });
   } catch (error) {
     // Send Error Response
     res.status(500).json({
-      error
+      message: "Something went wrong",
+      error: error.message,
     });
   }
 };
 
 //user Login
 exports.login = async (req, res, next) => {
-  const {
-    email_address,
-    password
-  } = req.body;
+  const { email_address, password } = req.body;
 
   //check for presence of email and password
   if (!email_address || !password) {
     return next(new CustomError("Please provide email and password", 400));
   }
 
-  const user = await userModel.findOne({
-    email_address
-  }).select("+password");
+  const user = await userModel
+    .findOne({
+      email_address,
+    })
+    .select("+password");
 
   if (!user) {
     return next(
