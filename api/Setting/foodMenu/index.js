9ca5b732b1a4db5cc5e7b31c9d15e2b869e58def
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-21 11:45:04 +0530

    Update index.js

--- api/Setting/foodMenu/index.js
+++ api/Setting/foodMenu/index.js
@@ -14,7 +14,7 @@ const foodMenuSchema = Schema({
   code: {
     type: String,
     maxlength: [50, "Maximum 50 charcters are permitted"],
-    minLength: [5, "code should have more than 5 character"],
+    minLength: [3, "code should have more than 5 character"],
     required: [true, "please enter code"],
     trim: true,
     default: null,
