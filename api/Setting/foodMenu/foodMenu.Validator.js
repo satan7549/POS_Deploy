ea1f7e0d071ac849b74622705a0a40adf072707e
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-07-21 11:39:16 +0530

    Update foodMenu.Validator.js

--- api/Setting/foodMenu/foodMenu.Validator.js
+++ api/Setting/foodMenu/foodMenu.Validator.js
@@ -1,100 +1,54 @@
 const Joi = require("joi");
 
 const foodMenuSchema = Joi.object({
-
-  name: Joi.string().max(50).required(),
-  code: Joi.string().max(50).required(),
+  name: Joi.string().max(50).required().trim(),
+  code: Joi.string().max(50).required().trim(),
   food_category: Joi.string().required(),
   ingredients: Joi.array()
     .items(
       Joi.object({
         ingredient: Joi.string().required(),
-        quantity: Joi.number().required(),
+        quantity: Joi.number().min(1).required(),
       })
     )
     .required(),
-  Dine_price: Joi.number().messages({
+  Dine_price: Joi.number().min(0).messages({
     "number.base": "Dine_price must be a number",
     "number.empty": "Dine_price is required",
     "any.required": "Please enter a Dine_price",
+    "number.min": "Dine_price should be greater than or equal to 0",
   }),
-  Takeaway_price: Joi.number().messages({
+  Takeaway_price: Joi.number().min(0).messages({
     "number.base": "Takeaway_price must be a number",
     "number.empty": "Takeaway_price is required",
     "any.required": "Please enter a Takeaway_price",
+    "number.min": "Takeaway_price should be greater than or equal to 0",
   }),
-  Delivery_price: Joi.number().messages({
-    "number.base": "Delivery_price must be a number",
-    "number.empty": "Delivery_price is required",
-    "any.required": "Please enter a Delivery_price",
-  }),
-  description: Joi.string().max(200).required(),
-  isVeg: Joi.boolean().required().messages({
-    "boolean.base": "IsVeg must be a boolean",
-    "any.required": "Please specify if the combo is vegetarian",
-  }),
-
-  isBeverage: Joi.boolean().required().messages({
-    "boolean.base": "IsBeverage must be a boolean",
-    "any.required": "Please specify if the combo is a beverage",
-  }),
-  outlet: Joi.string().required(),
-});
-
-
-
-const updateSchema = Joi.object({
-  
-  name: Joi.string().max(50).required(),
-  code: Joi.string().max(50).required(),
-  food_category: Joi.string().required(),
-  ingredients: Joi.array()
+  Delivery_price: Joi.array()
     .items(
       Joi.object({
-        ingredient: Joi.string().required(),
-        quantity: Joi.number().required(),
+        deliveryPartnerName: Joi.string().required(),
+        price: Joi.number().min(0).required(),
       })
     )
     .required(),
-  Dine_price: Joi.number().required().messages({
-    "number.base": "Dine_price must be a number",
-    "number.empty": "Dine_price is required",
-    "any.required": "Please enter a Dine_price",
-  }),
-  Takeaway_price: Joi.number().required().messages({
-    "number.base": "Takeaway_price must be a number",
-    "number.empty": "Takeaway_price is required",
-    "any.required": "Please enter a Takeaway_price",
-  }),
-  Delivery_price: Joi.number().required().messages({
-    "number.base": "Delivery_price must be a number",
-    "number.empty": "Delivery_price is required",
-    "any.required": "Please enter a Delivery_price",
-  }),
-
-  description: Joi.string().max(200).required(),
-
+  description: Joi.string().max(200).trim().default(null),
   isVeg: Joi.boolean().required().messages({
-    "boolean.base": "IsVeg must be a boolean",
+    "boolean.base": "isVeg must be a boolean",
     "any.required": "Please specify if the combo is vegetarian",
   }),
-
   isBeverage: Joi.boolean().required().messages({
-    "boolean.base": "IsBeverage must be a boolean",
+    "boolean.base": "isBeverage must be a boolean",
     "any.required": "Please specify if the combo is a beverage",
   }),
-  outlet: Joi.string().required(),
+  outlet: Joi.string().required().trim(),
+  del_status: Joi.string().valid("Live", "Deleted").default("Live"),
 });
 
 function validateFoodMenu(foodMenuData) {
   return foodMenuSchema.validate(foodMenuData);
 }
 
-function validateUpdate(updateData) {
-  return updateSchema.validate(updateData);
-}
-
 module.exports = {
   validateFoodMenu,
-  validateUpdate,
 };
