7fa3ea33053b8a12f012728b7ff5646b63b943fd
rajan-maury <rajan.maurya@furation.tech>
2023-07-22 12:17:30 +0530

    premade food changes

--- api/Setting/preFoodMade/index.js
+++ api/Setting/preFoodMade/index.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const preMadeFoodSchema = Schema({
-  nameFood: { type: String, maxlength: [50, "Maximum 50 charcters are permitted"],  minLength: [5, "persons should have more than 5 character"], required: [true, "please enter foodName"], trim: true, },
+  nameFood: {
+     type: String, 
+     maxlength: [50, "Maximum 50 charcters are permitted"], 
+      minLength: [5, "persons should have more than 5 character"], 
+      required: [true, "please enter foodName"], 
+      trim: true, 
+    },
+    code: {
+      type: String,
+      maxlength: [50, "Maximum 50 charcters are permitted"],
+      minLength: [3, "code should have more than 3 character"],
+      required: [true, "please enter code"],
+      trim: true,
+      default: null,
+      unique: true,
+    },
   category: { type: String, maxlength: [50, "Maximum 50 charcters are permitted"], minLength: [5, "waiter name should have more than 3 character"], required: [true, "please enter description"], trim: true, },
   purchasePrice: { type: Number, required: true },
   lowQAmt: { type: Number, required: true  },
